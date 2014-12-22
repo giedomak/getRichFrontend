8771e8f29f91dd311ee275d215953b1a9f9a39ef
Grabot <SanderKools@gmail.com>
2014-12-22 23:47:11 +0100

    yet another test thing with d3
    some simple bar chart using d3 and playing around, getting used to the
    new stuff and now able to start coding the actual code

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,41 +1,91 @@
-angular.module('getRichFrontendApp').controller('AboutCtrl', ['$scope', function($scope) {
-  $scope.model = {
-    test2: [
-    {x: 45, y: 45},
-    {x: 100, y: 120},
-    {x: 180, y: 15},
-    {x: 72, y: 145},
-    {x: 10, y: 66},
-    {x: 90, y: 95}
-    ]
-  };
+angular.module('getRichFrontendApp').controller('AboutCtrl', ['$scope', function($scope) 
+{
+  $scope.myData = 
+  [
+    {name: 'AngularJS', count: 300},
+    {name: 'D3.JS', count: 150},
+    {name: 'jQuery', count: 400},
+    {name: 'Backbone.js', count: 300},
+    {name: 'Ember.js', count: 100}
+    ];
 }]);
 
-angular.module('getRichFrontendApp').directive('cbCircleSquare', [function() {
-  return {
-    restrict: 'A',
-    template: '<circle ng-attr-cx="{{x}}" cy="100" r="25"></circle><rect ng-attr-x="{{x - 25}}" y="75" width="50" height="50" fill="none" stroke="blue"></rect>',
-    scope: {
-      'x': '=cbX'
-    }
-  };
-}]);
-
-angular.module('getRichFrontendApp').directive('cbScatter', [function() {
-  return {
-    restrict: 'E',
-    template: '<svg></svg>',
-    link: function(scope, element, attrs) {
-      scope.$watch(attrs.scatterData, function(scatterData) {
-        var svg = d3.select(element[0].firstChild);
-
-        svg.selectAll('circle')
-            .data(scatterData)
-          .enter().append('circle')
-            .attr('r', 3)
-            .attr('cx', function(d) {return d.x; })
-            .attr('cy', function(d) {return d.y; });
-      });
-    }
-  };
-}]);
+angular.module('getRichFrontendApp').directive('crD3Bars', [
+  function () {
+    return {
+      restrict: 'E',
+      scope: {
+        data: '='
+      },
+      link: function (scope, element) {
+        var margin = {top: 20, right: 20, bottom: 30, left: 40},
+          width = 480 - margin.left - margin.right,
+          height = 360 - margin.top - margin.bottom;
+        var svg = d3.select(element[0])
+          .append("svg")
+          .attr('width', width + margin.left + margin.right)
+          .attr('height', height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+ 
+        var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+        var y = d3.scale.linear().range([height, 0]);
+ 
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+ 
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(10);
+ 
+        //Render graph based on 'data'
+        scope.render = function(data) {
+          //Set our scale's domains
+          x.domain(data.map(function(d) { return d.name; }));
+          y.domain([0, d3.max(data, function(d) { return d.count; })]);
+          
+          //Redraw the axes
+          svg.selectAll('g.axis').remove();
+          //X axis
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+              
+          //Y axis
+          svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Count");
+              
+          var bars = svg.selectAll(".bar").data(data);
+          bars.enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.name); })
+            .attr("width", x.rangeBand());
+ 
+          //Animate bars
+          bars
+              .transition()
+              .duration(1000)
+              .attr('height', function(d) { return height - y(d.count); })
+              .attr("y", function(d) { return y(d.count); })
+        };
+ 
+         //Watch 'data' and run scope.render(newVal) whenever it changes
+         //Use true for 'objectEquality' property so comparisons are done on equality and not reference
+          scope.$watch('data', function(){
+              scope.render(scope.data);
+          }, true);  
+        }
+    };
+  }
+]);
