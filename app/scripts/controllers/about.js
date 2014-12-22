fe7128b374a796c6e6fa0952b41752910deb9f8a
Grabot <SanderKools@gmail.com>
2014-12-22 23:01:05 +0100

    simple scatter example
    simple example to use basic d3 capabilities

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,6 +1,13 @@
 angular.module('getRichFrontendApp').controller('AboutCtrl', ['$scope', function($scope) {
   $scope.model = {
-    test: [100, 200, 300]
+    test2: [
+    {x: 45, y: 45},
+    {x: 100, y: 120},
+    {x: 180, y: 15},
+    {x: 72, y: 145},
+    {x: 10, y: 66},
+    {x: 90, y: 95}
+    ]
   };
 }]);
 
@@ -10,6 +17,25 @@ angular.module('getRichFrontendApp').directive('cbCircleSquare', [function() {
     template: '<circle ng-attr-cx="{{x}}" cy="100" r="25"></circle><rect ng-attr-x="{{x - 25}}" y="75" width="50" height="50" fill="none" stroke="blue"></rect>',
     scope: {
       'x': '=cbX'
+    }
+  };
+}]);
+
+angular.module('getRichFrontendApp').directive('cbScatter', [function() {
+  return {
+    restrict: 'E',
+    template: '<svg></svg>',
+    link: function(scope, element, attrs) {
+      scope.$watch(attrs.scatterData, function(scatterData) {
+        var svg = d3.select(element[0].firstChild);
+
+        svg.selectAll('circle')
+            .data(scatterData)
+          .enter().append('circle')
+            .attr('r', 3)
+            .attr('cx', function(d) {return d.x; })
+            .attr('cy', function(d) {return d.y; });
+      });
     }
   };
 }]);
