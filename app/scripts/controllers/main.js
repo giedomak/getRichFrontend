6fc6b7ae48e74d035ee3fe139be14a4edfbb5ba5
Grabot <SanderKools@gmail.com>
2015-01-09 17:14:52 +0100

    Best commit
    Reading Json should work, so based on www.giedomak.nl:8008/data the
    graph will be drawn

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,39 +1,6 @@
 angular.module("getRichFrontendApp").controller('MainCtrl', function($rootScope, $scope, $http) {
   $rootScope.tab = "home"
 
-  //right now manually added dates, these will be given by the Json file as well later.
-  var d = [];
-  d[1] = new Date("September 1, 2013");
-  d[2] = new Date("September 2, 2013");
-  d[3] = new Date("September 3, 2013");
-  d[4] = new Date("September 4, 2013");
-  d[5] = new Date("September 5, 2013");
-  d[6] = new Date("September 6, 2013");
-  d[7] = new Date("September 7, 2013");
-  d[8] = new Date("September 8, 2013");
-  d[9] = new Date("September 9, 2013");
-  d[10] = new Date("September 10, 2013");
-  d[11] = new Date("September 11, 2013");
-  d[12] = new Date("September 12, 2013");
-  d[13] = new Date("September 13, 2013");
-  d[14] = new Date("September 14, 2013");
-  d[15] = new Date("September 15, 2013");
-  d[16] = new Date("September 16, 2013");
-  d[17] = new Date("September 17, 2013");
-  d[18] = new Date("September 18, 2013");
-  d[19] = new Date("September 19, 2013");
-  d[20] = new Date("September 20, 2013");
-  d[21] = new Date("September 21, 2013");
-  d[22] = new Date("September 22, 2013");
-  d[23] = new Date("September 23, 2013");
-  d[24] = new Date("September 24, 2013");
-  d[25] = new Date("September 25, 2013");
-  d[26] = new Date("September 26, 2013");
-  d[27] = new Date("September 27, 2013");
-  d[28] = new Date("September 28, 2013");
-  d[29] = new Date("September 29, 2013");
-  d[30] = new Date("September 30, 2013");
-  d[31] = new Date("September 31, 2013");
 
   $http.get("http://www.giedomak.nl:8008/data").success(function(response) 
     {
@@ -45,7 +12,7 @@ angular.module("getRichFrontendApp").controller('MainCtrl', function($rootScope,
       {
         $scope.data.push(
         {
-          x: d[i+1],
+          x: new Date(response[i].x),
           joy: parseFloat(response[i].joy),
           trust: parseFloat(response[i].trust),
           fear: parseFloat(response[i].fear),
@@ -54,7 +21,8 @@ angular.module("getRichFrontendApp").controller('MainCtrl', function($rootScope,
           disgust: parseFloat(response[i].disgust),
           anger: parseFloat(response[i].anger),
           anticipation: parseFloat(response[i].anticipation),
-          Stock: parseFloat(response[i].Stock)
+          Stock: parseFloat(response[i].Stock),
+          Prediction: parseFloat(response[i].prediction)
         });
       }
     });
@@ -115,6 +83,12 @@ angular.module("getRichFrontendApp").controller('MainCtrl', function($rootScope,
       thickness: "10px",
       color: "#000000",
       type: 'area'
+    }, {
+      y: 'Prediction',
+      axis: 'y2',
+      thickness: '10px',
+      color: '#FF0000',
+      type: 'area'
     }],
     tooltip: {
       mode: "scrubber",
@@ -124,9 +98,3 @@ angular.module("getRichFrontendApp").controller('MainCtrl', function($rootScope,
     }
   }
 });
-
-
-function MainCtrl($scope,$http) 
-{
-  
-}
