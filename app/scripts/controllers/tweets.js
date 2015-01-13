100136331e67cb53cc4737158fd50ec12f37a901
Grabot <SanderKools@gmail.com>
2015-01-13 15:17:02 +0100

    preperation?

--- app/scripts/controllers/tweets.js
+++ app/scripts/controllers/tweets.js
@@ -1,8 +1,3 @@
 angular.module("getRichFrontendApp").controller('TweetCtrl', function($rootScope, $scope, $http) {
   $rootScope.tab = "tweets"
-
-  function updateText(text)
-  {
-    document.getElementById("result").innerHTML =text;
-  }
 });
